d89e86545fc6f2af71d9f41292e312be3268dc6d
abhijeet117 <abhijeetkumarsah92@gmail.com>
2026-01-31 15:34:33 +0530

    Added Doc

---
+++ MAC-OS-Portfolio-2/src/App.jsx
@@ -0,0 +1,12 @@
+import "./app.scss"
+import Docs from "./components/Docs";
+
+const App = () => {
+  return (
+    <main className="main">
+         <Docs />
+    </main>
+  )
+}
+
+export default App;
---
+++ MAC-OS-Portfolio-2/src/components/Docs.jsx
@@ -0,0 +1,17 @@
+import "./Dock.scss"
+const Docs = () => {
+  return (
+    <footer className="dock">
+      <div className="icon pdf"><img src="/Doc-icons/pdf.svg"/></div>
+      <div className="icon spotify"><img src="/Doc-icons/spotify.svg"/></div>
+      <div className="icon github"><img src="/Doc-icons/github.svg"/></div>
+      <div className="icon mail"><img src="/Doc-icons/mail.svg"/></div>
+      <div className="icon link"><img src="/Doc-icons/link.svg"/></div>
+      <div className="icon note"><img src="/Doc-icons/note.svg"/></div>
+      <div className="icon calender"><img src="/Doc-icons/calender.svg"/></div>
+      <div className="icon cli"><img src="/Doc-icons/cli.svg"/></div>
+    </footer>
+  )
+}
+
+export default Docs
