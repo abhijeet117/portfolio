2b420b8d9932b5af87f3465ec209cd27d756407e
abhijeet117 <abhijeetkumarsah92@gmail.com>
2026-02-10 12:52:59 +0530

    Added MacWindows Terminal OnlyStructure

--- MAC-OS-Portfolio-2/src/App.jsx
+++ MAC-OS-Portfolio-2/src/App.jsx
@@ -1,12 +1,14 @@
 import "./app.scss";
 import Docs from "./components/Docs";
 import Navbar from "./components/Navbar";
+import MacWindow from "./components/windows/MacWindow";
 
 const App = () => {
   return (
     <main className="main">
       <Docs />
       <Navbar />
+      <MacWindow/>
     </main>
   );
 };
---
+++ MAC-OS-Portfolio-2/src/components/windows/MacWindow.jsx
@@ -0,0 +1,33 @@
+import { Rnd } from "react-rnd"
+import "./MacWindows.scss"
+
+const MacWindow = () => {
+  return (
+    <Rnd>
+      <div className="windows">
+        <div className="nav">
+          <div className="dots">
+            <div className="dot red">
+
+            </div>
+            <div className="dot yellow">
+
+            </div>
+            <div className="dot green">
+
+            </div>
+          </div>
+
+          <div className="tittle">
+            <p>Terminal — Abhijeet . -zsh — 80 x 64</p>
+          </div>
+        </div>
+        <div className="main-content">
+
+        </div>
+      </div>
+    </Rnd>
+  )
+}
+
+export default MacWindow
